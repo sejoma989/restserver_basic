4499c5e7f719558037843f9527492e2c5f007e7a
Jose Valencia - VaioSSD <sejoma989@gmail.com>
2022-10-27 23:40:54 -0500

    CRUD Usuarios - 27-10-2022

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -11,18 +11,18 @@ const usuariosGet = async(req = request, res = response) => {
     const { limite = 5, desde = 0 } = req.query;
     const query = { estado: true }
 
-    const usuarios = await Usuario.find(query)
-        .skip(Number(desde))
-        .limit(Number(limite));
+    // const usuarios = await Usuario.find(query)
+    //     .skip(Number(desde))
+    //     .limit(Number(limite));
 
-    const total = await Usuario.countDocuments(query)
+    // const total = await Usuario.countDocuments(query)
 
-    // const [ total, usuarios ] = await Promise.all([
-    //     Usuario.countDocuments(query),
-    //     Usuario.find(query)
-    //         .skip(Number(desde))
-    //         .limit(Number(limite))
-    // ]);
+    const [ total, usuarios ] = await Promise.all([
+        Usuario.countDocuments(query),
+        Usuario.find(query)
+            .skip(Number(desde))
+            .limit(Number(limite))
+    ]);
 
     res.json({
         total,
