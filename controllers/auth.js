02c891525b09e9609c3b2a9a45358943e6dbd4bb
Jose Valencia - VaioSSD <sejoma989@gmail.com>
2022-12-24 01:54:27 -0500

    Fin seccino 11 - version 4.0.0

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ import { response } from 'express';
 import bcrypt from 'bcrypt';
 import Usuario from '../models/usuario.js';
 import { generarJWT } from '../helpers/generar-jwt.js';
+import { googleVerify } from '../helpers/google-verify.js';
 
 const login = async(req, res = response) => {
 
@@ -49,6 +50,55 @@ const login = async(req, res = response) => {
 
 }
 
+const googleSignIn = async(req, res = response ) => {
+
+    const { id_token } = req.body;
+
+
+    try {
+
+        const { correo, nombre, img } = await googleVerify( id_token );
+        // console.log(googleUser)
+        let usuario = await Usuario.findOne({ correo });
+
+        if ( !usuario ) {
+            const data = {
+                nombre, 
+                correo, 
+                password: ':p',
+                img,
+                google: true
+            };
+
+            usuario = new Usuario( data );
+            await usuario.save()
+        }
+
+        if ( !usuario.estado ) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador - Usuario bloqueado'
+            });
+        }
+
+        const token = await generarJWT( usuario.id );
+
+        res.json({
+            usuario,
+            token
+        }); 
+
+    } catch (error) {
+        res.status(400).json({
+            ok: false,
+            msg: 'El token no se pudo verificar'
+        })
+    }
+
+    
+
+}
+
 export {
-    login
+    login,
+    googleSignIn
 }
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { check } from 'express-validator';
 
-import { login } from '../controllers/auth.js';
+import { googleSignIn, login } from '../controllers/auth.js';
 import { validarCampos } from "../middlewares/validar-campos.js";
 
 const router = Router();
@@ -11,5 +11,10 @@ router.post('/login',[
     check('password', 'La contraseña es obligatoria!').not().isEmpty(),
     validarCampos
 ],login );
+
+router.post('/google',[
+    check('id_token', 'id_token es necesario!').not().isEmpty(),
+    validarCampos
+], googleSignIn );
 
 export default router;
